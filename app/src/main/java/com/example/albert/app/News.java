a6a7a0569dd819cf68a8f3f3829a5543b48d0c7d
Albert Pratomo <i406448@fhict.nl>
2018-08-31 11:19:37 +0200

    Main, Schedule, and News activity

--- app/src/main/java/com/example/albert/app/News.java
+++ app/src/main/java/com/example/albert/app/News.java
@@ -2,15 +2,12 @@ package com.example.albert.app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.CalendarView;
-import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class News extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_news);
     }
 }
