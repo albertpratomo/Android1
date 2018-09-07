738a75bb52f6a2e033a8edf87547529d266d91ac
Albert Pratomo <i406448@fhict.nl>
2018-09-07 10:05:58 +0200

    Added CalendarView and onSelectedDayChange event on ScheduleActivity.

--- app/src/main/java/com/example/albert/app/Schedule.java
+++ app/src/main/java/com/example/albert/app/Schedule.java
@@ -1,13 +1,32 @@
 package com.example.albert.app;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.TextView;
 
 public class Schedule extends AppCompatActivity {
+
+    private CalendarView cv;
+    private TextView tv;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule);
+
+        cv = (CalendarView) findViewById(R.id.cv);
+        tv = (TextView) findViewById(R.id.tv);
+
+        cv.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                String date = dayOfMonth + "/" + (month + 1) + "/" + year;
+                tv.setText(date);
+            }
+        });
+
     }
 }
