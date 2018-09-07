1bf9d9943a7d0599286f326e3574afdd89098313
Albert Pratomo <i406448@fhict.nl>
2018-09-07 09:47:28 +0200

    Added button click event.

--- app/src/main/java/com/example/albert/app/MainActivity.java
+++ app/src/main/java/com/example/albert/app/MainActivity.java
@@ -2,15 +2,32 @@ package com.example.albert.app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView tv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv = findViewById(R.id.s);
+        tv.setText("Alo");
 
+        Button b = findViewById(R.id.button2);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tv.setText("Button2 clicked");
+            }
+        });
+    }
+
+    public void changeTv(View v){
+        tv.setText("Button clicked");
     }
 }
